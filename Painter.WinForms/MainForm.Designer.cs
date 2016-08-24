e7a35ee9bcfbdd98f4b0b557624d9f44b19e17e3
Vdovin Anton <antonvdovin@outlook.com>
2016-08-24 16:29:02 +0300

    prepare main form

---
+++ Painter.WinForms/MainForm.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Painter.WinForms
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.ButtonSave = new System.Windows.Forms.Button();
+            this.ButtonNew = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(-1, 397);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(827, 23);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // ButtonSave
+            // 
+            this.ButtonSave.Image = global::Painter.WinForms.Properties.Resources.Save_icon;
+            this.ButtonSave.Location = new System.Drawing.Point(51, 3);
+            this.ButtonSave.Name = "ButtonSave";
+            this.ButtonSave.Size = new System.Drawing.Size(33, 30);
+            this.ButtonSave.TabIndex = 3;
+            this.ButtonSave.UseVisualStyleBackColor = true;
+            // 
+            // ButtonNew
+            // 
+            this.ButtonNew.Image = global::Painter.WinForms.Properties.Resources.Files_New_File_icon;
+            this.ButtonNew.Location = new System.Drawing.Point(12, 3);
+            this.ButtonNew.Name = "ButtonNew";
+            this.ButtonNew.Size = new System.Drawing.Size(33, 30);
+            this.ButtonNew.TabIndex = 2;
+            this.ButtonNew.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(51, 39);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(710, 352);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(823, 418);
+            this.Controls.Add(this.ButtonSave);
+            this.Controls.Add(this.ButtonNew);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.Text = "Painter";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button ButtonNew;
+        private System.Windows.Forms.Button ButtonSave;
+    }
+}
+
